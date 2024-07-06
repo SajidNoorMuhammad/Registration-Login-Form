18713adea3c7088fb543875bbf832a6b87c00e93
Sajid Noor Muhammad <snoormuhammad37@gmail.com>
2024-07-07 02:34:29 +0500

    make changes.

--- app.js
+++ app.js
@@ -37,13 +37,49 @@ function signup() {
     fullName.value = "";
     userName.value = "";
     email.value = "";
-    phone.value= "";
-    password.value= "";
-    repassword.value= "";
-    address.value= "";
-    city.value= "";
+    phone.value = "";
+    password.value = "";
+    repassword.value = "";
+    address.value = "";
+    city.value = "";
 }
 
-function login(){
-    // location.assign("https://github.com/SajidNoorMuhammad/Registration-Login-Form")
+function login() {
+    document.getElementById("box").style.display = "none";
+    document.getElementById("loginbox").style.display = "block";
 }
+
+function register() {
+    document.getElementById("box").style.display = "block";
+    document.getElementById("loginbox").style.display = "none";
+}
+
+function loginPage() {
+    var email = document.getElementById("l-email");
+    var username = document.getElementById("l-username");
+    var password = document.getElementById("l-password");
+    var users = getusers();
+    var saveuser = {};
+
+    for (var i = 0; i < users.length; i++) {
+        if (email.value === users[i].email) {
+            saveuser = users[i]
+            break;
+        }
+    }
+
+    if(saveuser.email){
+        if(saveuser.password === password.value){
+            location.assign("https://sajidnoormuhammad.github.io/Complete-Car-Website-b-/")
+        }else{
+            alert("Invalid Password")
+        }
+    }else{
+        alert("User Not Found")
+    }
+
+    email.value= "";
+    password.value= "";
+    username.value="";
+}
+
